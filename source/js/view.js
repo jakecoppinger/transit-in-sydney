69a49e592591a40e86084a8c67603f9ca3575595
Jake Coppinger <jake@jakecoppinger.com>
2016-04-29 11:21:46 +1000

    Added circle drawing function

--- source/js/view.js
+++ source/js/view.js
@@ -8,7 +8,17 @@ jake@jakecoppinger.com
 
 var view = function(p) {
     p.drawTransitTriangle = function(color, tip) {
+        p.push();
         p.fill(color);
         p.triangle(0, 0, 0, p.windowHeight, tip.x, tip.y);
+        p.pop();
     };
+
+    p.drawCircle = function(color,position,diameter) {
+        p.push();
+        p.fill(color);
+        p.ellipse(position.x,position.y,diameter,diameter);
+        p.pop();
+    }
+
 };
