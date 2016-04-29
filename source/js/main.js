f159bedc24fe8b926f54b2487ae82c3b119e0094
Jake Coppinger <jake@jakecoppinger.com>
2016-04-29 11:22:21 +1000

    Using dicts rather that class objects for points

--- source/js/main.js
+++ source/js/main.js
@@ -48,34 +48,43 @@ var main = function(p) {
 
         var xTriangleScaler = 6 / 10;
 
-        var yellowTriangle = new p.Point({
+        var yellowPos = {
             x: p.pointDistance(mousePos, bottomLeft) * xTriangleScaler,
             y: p.windowHeight * (1 / 5)
-        });
+        };
 
-        var greenTriangle = new p.Point({
+        var greenPos = {
             x: p.pointDistance(mousePos, bottomRight) * xTriangleScaler,
             y: p.windowHeight * (2 / 5)
-        });
+        };
 
-        var blueTriangle = new p.Point({
+        var bluePos = {
             x: p.pointDistance(mousePos, topLeft) * xTriangleScaler,
             y: p.windowHeight * (3 / 5)
-        });
+        };
 
-        var pinkTriangle = new p.Point({
+        var pinkPos = {
             x: p.pointDistance(mousePos, topRight) * xTriangleScaler,
             y: p.windowHeight * (4 / 5)
-        });
+        };
 
         p.background(background);
         p.noStroke();
         p.blendMode(p.MULTIPLY);
 
-        p.drawTransitTriangle(blue, blueTriangle);
-        p.drawTransitTriangle(pink, pinkTriangle);
-        p.drawTransitTriangle(yellow, yellowTriangle);
-        p.drawTransitTriangle(orange, greenTriangle);
+        p.drawTransitTriangle(blue, bluePos);
+        p.drawTransitTriangle(pink, pinkPos);
+        p.drawTransitTriangle(yellow, yellowPos);
+        p.drawTransitTriangle(orange, greenPos);
+
+
+        bluePos.x += 50;
+        p.drawCircle(blue,bluePos,100);
+
+
+
+
+
     };
 
 
