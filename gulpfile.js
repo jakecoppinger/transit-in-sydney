4ae9ecdc3788c28d4685a920fcaecb201fb02181
Jake Coppinger <jake@jakecoppinger.com>
2016-05-03 22:24:57 +1000

    Removed sass from gulpfile (not supported on Node 6 yet

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ jake@jakecoppinger.com
 */
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+// var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var notify = require('gulp-notify');
